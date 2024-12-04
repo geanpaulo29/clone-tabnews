a8a815bcf224c74df06595e9dfd048e974b0f155
geanpaulo <138536945+geanpaulo29@users.noreply.github.com>
2024-12-04 14:29:28 +0000

    adiciona scripts lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
-return <h1>Compro guitarra Fender americana usada!</h1>
+  return <h1>Compro guitarra Fender americana usada!</h1>;
+}
+
+function Teste() {
+  console.log("teste");
+}
+
+function Teste2() {
+  console.log("teste2");
 }
 
 export default Home;
-
